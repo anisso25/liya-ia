6d28c8fadfa0da15af2f9e5db1527384554b9cef
Mr.Robot <macbookpro@MacBook-Pro-de-MacBook.local>
2023-11-18 23:58:13 +0100

    update

--- ia.py
+++ ia.py
@@ -1,4 +1,34 @@
 import speech_recognition as sr
+import requests
+from datetime import datetime
+
+# Clé API OpenWeatherMap (obtenez la vôtre sur https://openweathermap.org/)
+api_key = "04064b770955c32f56f17d1e2084be4d"
+
+# Fonction pour obtenir l'heure actuelle
+
+
+def get_current_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    return current_time
+
+# Fonction pour obtenir la météo par ville
+
+
+def get_weather(city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": api_key, "lang": "fr", "units": "metric"}
+    response = requests.get(base_url, params=params)
+    weather_data = response.json()
+
+    if "weather" not in weather_data:
+        return f"Impossible d'obtenir les données météorologiques pour {city}."
+
+    description = weather_data["weather"][0]["description"]
+    temperature = weather_data["main"]["temp"]
+    return f"La météo à {city} est {description}. La température est d'environ {temperature} degrés Celsius."
+
 
 # Créer un objet Recognizer
 recognizer = sr.Recognizer()
@@ -17,35 +47,39 @@ while True:
             text = recognizer.recognize_google(audio, language="fr-FR")
             print("Vous avez dit: {}".format(text))
 
-            # Si l'utilisateur dit "jarvis", activez l'écoute
-            if "jarvis" in text.lower():
-                print("Activation de l'écoute...")
-                listening = True
-
-            # Si l'écoute est activée, traitez les commandes
+            # Si l'écoute est activée, traitez la commande
             if listening:
-                try:
-                    audio = recognizer.listen(source)
-                    command = recognizer.recognize_google(
-                        audio, language="fr-FR")
-                    print("Commande détectée: {}".format(command))
+                # Ajoutez cette ligne pour déboguer
+                print("Commande audio: {}".format(text))
 
-                    # Traitez la commande ou ajoutez d'autres conditions pour sortir de l'écoute
-                    if "arrêt" in command.lower():
+                if "arrêt" in text.lower():
+                    print("Arrêt de l'écoute.")
+                    listening = False
+                    break  # Sortez de la boucle
+
+                elif "quelle heure est-il" in text.lower():
+                    current_time = get_current_time()
+                    print("Il est actuellement {}".format(current_time))
+
+                elif "comment est la météo à" in text.lower():
+                    city = text.split("comment est la météo à")[-1].strip()
+                    weather_response = get_weather(city)
+                    print(weather_response)
+
+                    # Ajoutez une vérification pour décider d'arrêter ou continuer
+                    if "Impossible d'obtenir les données météorologiques" in weather_response:
                         print("Arrêt de l'écoute.")
                         listening = False
+                        break
 
-                    # Ajoutez d'autres conditions de commande ici
-
-                except sr.UnknownValueError:
-                    print("Google Web Speech API n'a pas pu comprendre l'audio")
-                except sr.RequestError as e:
-                    print(
-                        "Erreur lors de la requête à Google Web Speech API; {0}".format(e))
-                    break  # Sortez de la boucle en cas d'erreur de requête
+            # Activez l'écoute si nécessaire
+            if "début" in text.lower():
+                print("Activation de l'écoute...")
+                listening = True
 
         except sr.UnknownValueError:
             print("Google Web Speech API n'a pas pu comprendre l'audio")
         except sr.RequestError as e:
             print(
                 "Erreur lors de la requête à Google Web Speech API; {0}".format(e))
+            break  # Sortez de la boucle en cas d'erreur de requête
